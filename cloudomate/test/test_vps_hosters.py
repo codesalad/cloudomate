acf636f575cba889e0ad323be3b6d3c88f859dae
HvanVeltom <H.J.M.vanVeltom@student.tudelft.nl>
2018-06-29 09:37:53 +0200

    removed some comments

--- cloudomate/test/test_vps_hosters.py
+++ cloudomate/test/test_vps_hosters.py
@@ -27,11 +27,11 @@ standard_library.install_aliases()
 providers = [
     (LineVast,),
     (TwoSync,),
-    #(BlueAngelHost,),
-    #(CCIHosting,), # TODO CCIHosting doesn't use coinbase anymore instead it uses coinpayments the code needs to be updated.
-    #(CrownCloud,),
-    #(Pulseservers,),
-    #(UndergroundPrivate,),# TODO find a way to combine the url and the invoice to be able to go to the payment page
+    (BlueAngelHost,),
+    (CCIHosting,), # TODO CCIHosting doesn't use coinbase anymore instead it uses coinpayments the code needs to be updated.
+    (CrownCloud,),
+    (Pulseservers,),
+    (UndergroundPrivate,),# TODO find a way to combine the url and the invoice to be able to go to the payment page
 ]
 
 
